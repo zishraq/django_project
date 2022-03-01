30bbb1c4ad0ff41e3f7b3d02b8222d16821d0ca7
zishraq <z.ishraq@gmail.com>
2022-03-01 10:30:51 +0600

    Test changes

--- advising_portal/models.py
+++ advising_portal/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+
+class Department(models.Model):
+    department_id = models.CharField(max_length=100)
+    # department_name =
+
+
+
 class Courses(models.Model):
     course_code = models.CharField(max_length=100)
     course_title = models.CharField(max_length=100)
