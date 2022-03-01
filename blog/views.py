7905ac9eed19b573e30a1b6cda0ed9fcbe7009e6
zishraq <z.ishraq@gmail.com>
2022-03-01 06:11:22 +0600

    Tutorial finished upto 7th video

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Zuhair Ishraq',
-        'title': 'Blog Post 1',
-        'content': '1 post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Marcus Aurelius',
-        'title': 'Blog Post 2',
-        'content': '2 post content',
-        'date_posted': 'August 30, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
