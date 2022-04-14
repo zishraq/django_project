2323ce7cd613babad6083a8cb4dc949cceeb0bf9
TanvirMobasshir <mdtanvirmobasshir@gmail.com>
2022-04-14 13:28:39 +0600

    checks if mail address valid

--- registration/registration.py
+++ registration/registration.py
@@ -1,8 +1,22 @@
-import smtplib
+import smtplib, re
 from random import randint
 
 
 def send_otp(receiver_mail: str):
+
+    otp_data = {
+        'mail_address': receiver_mail,
+        'valid_mail': False,
+        'otp': None
+    }
+
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    if re.fullmatch(regex, receiver_mail):
+        otp_data['valid_mail'] = True
+    else:
+        print("invalid email address")
+        return otp_data
 
     otp = randint(10000, 999999)
     email_text = f"""\
@@ -29,16 +43,12 @@ ABCD University
         smtp_server.login('onlineadvisingportal@gmail.com', '123456Seven')
         smtp_server.sendmail('onlineadvisingportal@gmail.com', receiver_mail, email_text)
         smtp_server.close()
+        otp_data['otp'] = otp
         print("Email sent successfully!")
     except Exception as ex:
         print("Something went wrong….", ex)
 
-    otp_data = {
-        'mail_address': receiver_mail,
-        'otp': otp
-    }
-
     return otp_data
 
 
-send_otp('mdtanvirmobasshir@gmail.com')
+print(send_otp('mdtanvirmobasshir@gmail.com'))
