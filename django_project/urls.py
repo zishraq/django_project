321de1c8915ef0dd178f15aed517e15ca6f01165
zishraq <z.ishraq@gmail.com>
2022-04-21 00:17:16 +0600

    Login template changed

--- django_project/urls.py
+++ django_project/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.profile_view, name='profile'),
     path('profile/', user_views.profile_view, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login_old.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('activate/', user_views.activate_student_profile_view, name='activate'),
     path('set-password/<otp_id>', user_views.set_password_view, name='set_password'),
