982fc5e8b8b7c2e4694bc25a9f945ccafa1c76e4
zishraq <z.ishraq@gmail.com>
2022-04-28 02:31:08 +0600

    code optimized

---
+++ advising_portal/static/advising_portal/js/my_scripts.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        if (i === 0) {
+            continue;
+        }
+        td = tr[i].getElementsByTagName("td")[0];
+
+        if (td && td.colSpan !== 7) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
