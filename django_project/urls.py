f1c33fe600f38dcd5d0442c27ebbe9c2eabb8abf
zishraq <z.ishraq@gmail.com>
2022-05-15 19:31:07 +0600

    activate form updated

--- django_project/urls.py
+++ django_project/urls.py
@@ -8,7 +8,7 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profile/', user_views.profile_view, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', user_views.login_view, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('activate/', user_views.activate_student_profile_view, name='activate'),
     path('set-password/<otp_id>', user_views.set_password_view, name='set_password'),
